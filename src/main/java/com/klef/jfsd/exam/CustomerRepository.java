7109ba3c5f39674ec6c18e37ad9e0e59962a37f4
2200030293 <140525168+2200030293@users.noreply.github.com>
2024-12-07 08:36:49 +0530

    V1.0

---
+++ src/main/java/com/klef/jfsd/exam/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.klef.jfsd.exam;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/com/klef/jfsd/exam/JfsDendLabExamApplication.java
@@ -0,0 +1,13 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsDendLabExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsDendLabExamApplication.class, args);
+	}
+
+}
